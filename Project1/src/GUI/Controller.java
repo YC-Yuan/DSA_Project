c97dca5edb05cc2aa27414004188406bc5f8e96b
Yuan Yicong <19302010020@fudan.edu.cn>
2020-10-29 00:36:31 +0800

    GUI starting

--- Project1/src/GUI/Controller.java
+++ Project1/src/GUI/Controller.java
@@ -1,4 +1,40 @@
 package GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class Controller {
+    public Button buttonSource;
+    public Button buttonDestination;
+    public Button buttonCompress;
+    public Button buttonDecompress;
+
+
+    public Text textTitle;
+    public Text textRatio;
+    public Text textSize;
+    public Text textSpeed;
+    public Text textTime;
+    public Text textOriSize;
+
+    @FXML
+    public void clickSource() {
+
+    }
+
+    @FXML
+    public void clickDestination() {
+
+    }
+
+    @FXML
+    public void clickCompress() {
+
+    }
+
+    @FXML
+    public void clickDecompress() {
+
+    }
 }
