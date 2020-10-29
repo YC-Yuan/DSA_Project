f16581c5a240883d6db91b18fcb56b640cc54fd4
Yuan Yicong <19302010020@fudan.edu.cn>
2020-10-29 13:16:14 +0800

    GUI writing

--- Project1/src/GUI/Controller.java
+++ Project1/src/GUI/Controller.java
@@ -1,8 +1,16 @@
 package GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.List;
 
 public class Controller {
     public Button buttonSource;
@@ -18,14 +26,17 @@ public class Controller {
     public Text textTime;
     public Text textOriSize;
 
-    @FXML
-    public void clickSource() {
+    public AnchorPane rootPane;
 
-    }
+    private String destinationPath;
+    private String originalPath;
 
     @FXML
     public void clickDestination() {
-
+        DirectoryChooser directoryChooser=new DirectoryChooser();
+        directoryChooser.setTitle("Choose compress/decompress destination");
+        File destinationFolder = directoryChooser.showDialog(rootPane.getScene().getWindow());
+        destinationPath=destinationFolder.getPath();
     }
 
     @FXML
@@ -36,5 +47,15 @@ public class Controller {
     @FXML
     public void clickDecompress() {
 
+    }
+
+    public void clickFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a file/folder");
+        //File originalFile = fileChooser.showOpenDialog(rootPane.getScene().getWindow());
+
+        DirectoryChooser directoryChooser=new DirectoryChooser();
+        directoryChooser.setTitle("Choose a folder");
+        directoryChooser.showDialog(rootPane.getScene().getWindow());
     }
 }
