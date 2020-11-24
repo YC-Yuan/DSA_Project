20711e871b7159107323737ef869c59998902c98
Yuan Yicong <19302010020@fudan.edu.cn>
2020-11-24 17:59:59 +0800

    try floyd

---
+++ Project2/src/Floyd.java
@@ -0,0 +1,13 @@
+public class Floyd {
+    public static void run(){
+        for (int i=0;i<312;i++){//在i轮纳入0号station
+            for(int start=0;i<312;i++){
+                for (int end=0;i<312;i++){//迭代更新从start到end站点间的路径
+                    //如果start->i+i->end比start->end更小，则更新路线，否则什么也不做
+
+                    //路线更新为：先从start到i，再从i到end，如果前后无换成，则省略更新点
+                }
+            }
+        }
+    }
+}
--- Project2/src/Path.java
+++ Project2/src/Path.java
@@ -3,5 +3,4 @@ import java.util.Vector;
 public class Path {
     public int length=Integer.MAX_VALUE;
     public Vector<Station> path=new Vector<>();
-
 }
