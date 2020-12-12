6da00bb5fa90b626df911f81c101d1c7a52e23b1
Yuan Yicong <19302010020@fudan.edu.cn>
2020-12-12 21:52:38 +0800

    try 3\4

--- Project3/src/Main.java
+++ Project3/src/Main.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) {
+
+    public static TreeMap<String, Long> map = new TreeMap<>();
+
+
+    public static void main(String[] args) {
+
         Scanner cin = new Scanner(System.in);
 
         String input;
@@ -10,40 +15,80 @@ public class Main {
         String key;
         long value;
 
-        HashMap<String,Long> map=new HashMap<>();
-
         int index;
 
-        cin.nextLine();
+        String number = cin.nextLine();
+        int num = Integer.parseInt(number);
+
+        SortedMap<String, Long> sortedMap;
 
 
-        while (cin.hasNext()) {
-            input=cin.nextLine();
-            index=input.indexOf(" ");
-            method=input.substring(0,index++);
-            content=input.substring(index);
+        while (num > 0) {
+            input = cin.nextLine();
+            index = input.indexOf(" ");
+            method = input.substring(0, index++);
+            content = input.substring(index);
 
-            switch (method){
+            switch (method) {
                 case "PUT":
-                    index=content.indexOf(" ");
-                    key=content.substring(0,index++);
-                    value=Long.parseLong(content.substring(index));
-                    map.put(key,value);
+                    index = content.indexOf(" ");
+                    key = content.substring(0, index++);
+                    value = Long.parseLong(content.substring(index));
+                    map.put(key, value);
                     break;
                 case "ADD":
-                    index=content.indexOf(" ");
-                    key=content.substring(0,index++);
-                    value=Long.parseLong(content.substring(index));
-                    value+=map.get(key);
-                    map.put(key,value);
+                    index = content.indexOf(" ");
+                    key = content.substring(0, index++);
+                    value = Long.parseLong(content.substring(index));
+                    value += map.get(key);
+                    map.put(key, value);
                     break;
                 case "QUERY":
                     System.out.println(map.get(content));
                     break;
                 case "DEL":
-                    map.put(content,(long)0);
+                    map.put(content, 0L);
                     break;
+                case "ADDBEGINWITH":
+                    index = content.indexOf(" ");
+                    key = content.substring(0, index++);
+                    value = Long.parseLong(content.substring(index));
 
+                    sortedMap = getSortedMap(key);
+                    for (String aKey : sortedMap.keySet()
+                    ) {
+                        map.put(aKey, map.get(aKey) + value);
+                    }
+                    break;
+                case "QUERYBEGINWITH":
+                    sortedMap = getSortedMap(content);
+                    long sum = 0;
+                    for (String aKey : sortedMap.keySet()
+                    ) {
+                        sum += map.get(aKey);
+                    }
+                    System.out.println(sum);
+                    break;
+                case "DELBEGINWITH":
+                    sortedMap = getSortedMap(content);
+                    for (String aKey : sortedMap.keySet()
+                    ) {
+                        map.put(aKey, 0L);
+                    }
+                    break;
+            }
+            num--;
+        }
+    }
+
+    public static SortedMap<String, Long> getSortedMap(String key) {
+        if (key.charAt(key.length() - 1) == 'z') {
+            return map.tailMap(key);
+        } else {
+            if (key.length() == 1) return map.subMap(key, Character.toString((char) (key.charAt(0) + 1)));
+            else {
+                String newKey = key.substring(0, key.length() - 2) + (char) (key.charAt(key.length() - 1) + 1);
+                return map.subMap(key, newKey);
             }
         }
     }
---
+++ Project3/src/Test.java
@@ -0,0 +1,9 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Test {
+    public static void main(String[]args){
+        char a='a';
+        System.out.println(++a);
+    }
+}
